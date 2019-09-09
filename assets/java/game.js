2a167f7737049987a8f9e21e731015ab5109ab0c
Kemp Atkinson <kempatkinson@gmail.com>
2019-09-09 15:28:33 -0500

    first little go

---
+++ assets/java/game.js
@@ -0,0 +1,25 @@
+var Chosen;
+
+var hotdog = {
+    "hp": 100 ,
+    "ap": 100 ,
+    "cap": 100 ,
+}
+
+var stinky = {
+    "hp": 100 ,
+    "ap": 100 ,
+    "cap": 100 ,
+}
+
+var howie = {
+    "hp": 100 ,
+    "ap": 100 ,
+    "cap": 100 ,
+}
+
+var organ = {
+    "hp": 100 ,
+    "ap": 100 ,
+    "cap": 100 ,
+}
