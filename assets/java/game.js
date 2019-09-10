a157cdc2936d59abf1d642033baab3985f53bf8e
Kemp Atkinson <kempatkinson@gmail.com>
2019-09-09 19:19:27 -0500

    nothing is working

--- assets/java/game.js
+++ assets/java/game.js
@@ -1,25 +1,40 @@
-var Chosen;
 
 var hotdog = {
-    "hp": 100 ,
-    "ap": 100 ,
-    "cap": 100 ,
+    "hp": 100,
+    "ap": 100,
+    "cap": 100,
 }
 
 var stinky = {
-    "hp": 100 ,
-    "ap": 100 ,
-    "cap": 100 ,
+    "hp": 100,
+    "ap": 100,
+    "cap": 100,
 }
 
 var howie = {
-    "hp": 100 ,
-    "ap": 100 ,
-    "cap": 100 ,
+    "hp": 100,
+    "ap": 100,
+    "cap": 100,
 }
 
 var organ = {
-    "hp": 100 ,
-    "ap": 100 ,
-    "cap": 100 ,
+    "hp": 100,
+    "ap": 100,
+    "cap": 100,
 }
+
+$(document).ready(function () {
+  //hot dog is selected as character, 
+     ($("#hotdog").onclick = function () {
+        $("#hotdog").attr("class", "hidden")
+        $("#stinky").attr("class", "hidden")
+        $("#organ").attr("class", "hidden")
+        $("#howie").attr("class", "hidden")
+        $("#selecthotdog").attr("class", "shown")
+    });
+
+
+
+
+
+});
