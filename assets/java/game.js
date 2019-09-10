3abcfa53722a324a296aeed303dd2d07a7e5c0ae
Kemp Atkinson <kempatkinson@gmail.com>
2019-09-09 20:22:27 -0500

    selected hero / enemy choices show up

--- assets/java/game.js
+++ assets/java/game.js
@@ -1,40 +1,80 @@
 
-var hotdog = {
-    "hp": 100,
-    "ap": 100,
-    "cap": 100,
-}
+// var hotdog = {
+//   "hp": 100,
+//   "ap": 100,
+//   "cap": 100,
+// }
 
-var stinky = {
-    "hp": 100,
-    "ap": 100,
-    "cap": 100,
-}
+// var stinky = {
+//   "hp": 100,
+//   "ap": 100,
+//   "cap": 100,
+// }
 
-var howie = {
-    "hp": 100,
-    "ap": 100,
-    "cap": 100,
-}
+// var howie = {
+//   "hp": 100,
+//   "ap": 100,
+//   "cap": 100,
+// }
 
-var organ = {
-    "hp": 100,
-    "ap": 100,
-    "cap": 100,
-}
+// var organ = {
+//   "hp": 100,
+//   "ap": 100,
+//   "cap": 100,
+// }
 
 $(document).ready(function () {
+  $("#selecthotdog").hide();
+  $("#selecthowie").hide();
+  $("#selectstinky").hide();
+  $("#selectorgan").hide();
+  $("#enemyhotdog").hide();
+  $("#enemyhowie").hide();
+  $("#enemystinky").hide();
+  $("#enemyorgan").hide();
   //hot dog is selected as character, 
-     ($("#hotdog").onclick = function () {
-        $("#hotdog").attr("class", "hidden")
-        $("#stinky").attr("class", "hidden")
-        $("#organ").attr("class", "hidden")
-        $("#howie").attr("class", "hidden")
-        $("#selecthotdog").attr("class", "shown")
-    });
-
-
-
-
+  ($("#hotdog").on("click", function () {
+    $("#hotdog").hide();
+    $("#howie").hide();
+    $("#stinky").hide();
+    $("#organ").hide();
+    $("#selecthotdog").show();
+    $("#enemyhowie").show();
+    $("#enemystinky").show();
+    $("#enemyorgan").show();
+  }));
+    //howie is selected
+    ($("#howie").on("click", function () {
+      $("#hotdog").hide();
+      $("#howie").hide();
+      $("#stinky").hide();
+      $("#organ").hide();
+      $("#selecthowie").show();
+      $("#enemyhotdog").show();
+      $("#enemystinky").show();
+      $("#enemyorgan").show();
+    }));
+    //organ
+    ($("#organ").on("click", function () {
+      $("#hotdog").hide();
+      $("#howie").hide();
+      $("#stinky").hide();
+      $("#organ").hide();
+      $("#selectorgan").show();
+      $("#enemyhotdog").show();
+      $("#enemystinky").show();
+      $("#enemyhowie").show();
+    }));
+    //stinky
+    ($("#stinky").on("click", function () {
+      $("#hotdog").hide();
+      $("#howie").hide();
+      $("#stinky").hide();
+      $("#organ").hide();
+      $("#selectstinky").show();
+      $("#enemyhotdog").show();
+      $("#enemyhowie").show();
+      $("#enemyorgan").show();
+    }));
 
 });
