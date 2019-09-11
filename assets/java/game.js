b5a6e906fae4b0b41ecf035e82c67c2e9c144751
Kemp Atkinson <kempatkinson@gmail.com>
2019-09-10 19:15:23 -0500

    f'ed up red enemy screen

--- assets/java/game.js
+++ assets/java/game.js
@@ -3,14 +3,16 @@ $(document).ready(function () {
         characters: [
             {
                 name: "Hotdog",
+                index: 0,
                 hp: 70,
                 ap: 110,
                 cap: 70,
                 image: "assets/images/hotdog.jpg",
-                
+
             },
             {
                 name: "Howie",
+                index: 1,
                 hp: 150,
                 ap: 90,
                 cap: 90,
@@ -18,6 +20,7 @@ $(document).ready(function () {
             },
             {
                 name: "Ruben",
+                index: 2,
                 hp: 100,
                 ap: 120,
                 cap: 60,
@@ -25,6 +28,7 @@ $(document).ready(function () {
             },
             {
                 name: "New Joe",
+                index: 3,
                 hp: 80,
                 ap: 120,
                 cap: 100,
@@ -40,33 +44,47 @@ $(document).ready(function () {
             image: ""
         },
 
-        printSelected: function() {
+        printSelected: function () {
             $('#selected-area').html("<img src='" + this.characterSelected.image + "' />")
 
         },
 
-        printEveryone: function() {
-            for( var i = 0; i < this.characters.length; i++) {
-            //make a div
-            var makeADiv = $('<div>');
-            //add classes and id to that div
-            makeADiv.attr('class', 'col-md-3 character');
-            //add text
-            makeADiv.text(this.characters[i].name);
-            //add image to div
-            var image = $('<img>');
-            image.attr('src', this.characters[i].image);
-
-            makeADiv.append(image);
-            //add to the page
-            $('.characters-here').append(makeADiv)
+        printEveryone: function () {
+            for (var i = 0; i < this.characters.length; i++) {
+                //make a div
+                var makeADiv = $('<div>');
+                //add classes and id to that div
+                makeADiv.attr('class', 'col-md-3 character');
+                //add text
+                makeADiv.text(this.characters[i].name);
+                //add image to div
+                var image = $('<img>');
+                image.attr('src', this.characters[i].image);
+                makeADiv.append(image);
+                makeADiv.append(this.characters[i].hp);
+                //add to the page
+                $('.characters-here').append(makeADiv)
             }
         },
     }
-    
+
 
     game.printEveryone();
-    });
+    
+    ($(".character").on("click", function () {
+        $(this).addClass("chosen");
+        $(this).removeClass("character")
+        $(this).detach().appendTo('.chosen-here');
+        $(".characters-here").addClass("enemy");
+        $(".characters-here").detach().appendTo('.enemies-here');
+    }))
+    ($(".enemy").on("click", function () {
+        $(this).addClass("defender");
+        $(this).detach().appendTo('.defenders-here');
+
+    }))
+
+    })
 
 
 
